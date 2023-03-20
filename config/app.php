38ff1d048b5721784df53b3eb84c957b5ea54f72
Rasal-Molla <rasalmolla159401@gmail.com>
2023-03-20 10:39:53 +0600

    Debagbar install

--- config/app.php
+++ config/app.php
@@ -199,7 +199,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Mckenziearts\Notify\LaravelNotifyServiceProvider::class
+        Mckenziearts\Notify\LaravelNotifyServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
