681de9c8ca342a23903faedf9a520dad34a198b4
Rasal-Molla <rasalmolla159401@gmail.com>
2023-03-25 17:54:49 +0600

    Slide Update

--- app/Http/Controllers/backend/SliderController.php
+++ app/Http/Controllers/backend/SliderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class SliderController extends Controller
 {
@@ -42,19 +43,40 @@ class SliderController extends Controller
         return redirect()->back();
     } // End method
 
-    public function slideEdit()
+    public function slideEdit($id)
     {
+        $slide = Slider::find($id);
+        return view('backend.pages.slider.edit_slider', compact('slide'));
     } // End method
 
-    public function cslidepdate()
+    public function slideUpdate(Request $request, $id)
     {
+        $request->validate([
+            'title' => 'required',
+            'short_title' => 'required',
+        ]);
+        $slide = Slider::find($id);
+        $image = $slide->image;
+        if ($request->hasFile('image')) {
+            $remove = public_path() . '/slides/' . $image;
+            File::delete($remove);
+            $image = date('Ymdhmi') . '.' . $request->file('image')->getClientOriginalExtension();
+            $request->file('image')->storeAs('/slides', $image);
+        }
+        $slide->update([
+            'title' => $request->title,
+            'short_title' => $request->short_title,
+            'image' => $image,
+        ]);
+        notify()->success('Slider Updated successfully');
+        return redirect()->route('all.slide');
     } // End method
 
     public function slideDelete($id)
     {
         $slide = Slider::find($id);
         $image = $slide->image;
-        unlink(public_path().'/slides/'.$image);
+        unlink(public_path() . '/slides/' . $image);
         Slider::findOrFail($id)->delete();
         notify()->success('Slider deleted successfully');
         return redirect()->back();
