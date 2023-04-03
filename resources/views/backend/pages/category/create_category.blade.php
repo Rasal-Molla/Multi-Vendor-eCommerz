3f29cd4a34ac6dbdd18fb07d694ee1776e2945eb
Rasal-Molla <rasalmolla159401@gmail.com>
2023-04-03 17:53:13 +0600

    udpate

--- resources/views/backend/pages/category/create_category.blade.php
+++ resources/views/backend/pages/category/create_category.blade.php
@@ -3,38 +3,39 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-lg-6">
-        <div class="card">
-            <div class="card-title">
-                <h4>Category Create Form</h4>
-            </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-title">
+                    <h4>Category Create Form</h4>
+                </div>
 
-            <div class="card-body">
-                <div class="basic-form">
-                    <form action="{{route('store.category')}}" method="post" enctype="multipart/form-data">
-                        @csrf
+                <div class="card-body">
+                    <div class="basic-form">
+                        <form action="{{ route('store.category') }}" method="post" enctype="multipart/form-data">
+                            @csrf
 
-                        @if ($errors->any())
-                            @foreach ($errors->all() as $message)
-                                <p class="alert alert-danger">{{$message}}</p>
-                            @endforeach
-                        @endif
+                            @if ($errors->any())
+                                @foreach ($errors->all() as $message)
+                                    <p class="alert alert-danger">{{ $message }}</p>
+                                @endforeach
+                            @endif
 
-                        @if(session()->has('message'))
-                            <p class="alert alert-success">{{session()->get('message')}}</p>
-                        @endif
+                            @if (session()->has('message'))
+                                <p class="alert alert-success">{{ session()->get('message') }}</p>
+                            @endif
 
-                        <div class="form-group">
-                            <label>Category Name</label>
-                            <input name="category_name" type="text" class="form-control" placeholder="Enter category name">
-                        </div>
-                        <div class="form-group">
-                            <label>Category Image</label>
-                            <input name="category_image" type="file" class="form-control">
-                        </div>
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
+                            <div class="form-group">
+                                <label>Category Name</label>
+                                <input name="category_name" type="text" class="form-control"
+                                    placeholder="Enter category name">
+                            </div>
+                            <div class="form-group">
+                                <label>Category Image</label>
+                                <input name="category_image" type="file" class="form-control">
+                            </div>
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
                     </form>
                 </div>
             </div>
